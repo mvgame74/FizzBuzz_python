c622738e080ce55f7171bb48a311b76e47fc8596
Maria Valero <mvalerogonzalez@gmail.com>
2021-10-29 10:32:29 +0000

    Third test working

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,6 @@
 
 def is_divisible_by_three(number):
   return number % 3 == 0
+  
+def is_divisible_by_five(number):
+  return number % 5 == 0
--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -6,3 +6,6 @@ def test_that_number_is_divisible_by_three():
 def test_that_number_is_NOT_divisible_by_three():
   assert fizzbuzz.is_divisible_by_three(1) == False
   
+def test_that_number_is_divisible_by_five():
+  assert fizzbuzz.is_divisible_by_five(5) == True
+     
