5f13453f81a71d591c7d90863d1be8c75e34d520
Maria Valero <mvalerogonzalez@gmail.com>
2021-10-29 12:34:44 +0000

    Nine tests passing

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,11 @@
 
 def is_divisible_by(number, divisor):
-     return number % divisor == 0
+  return number % divisor == 0
+  
+def says(number):
+  if is_divisible_by(number, 15):
+    return 'fizzbuzz'
+  if is_divisible_by(number, 3):
+    return 'fizz'
+  if is_divisible_by(number, 5):
+    return 'buzz'
--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -18,4 +18,13 @@ def test_that_number_is_divisible_by_three_and_five():
 def test_that_number_is_NOT_divisible_by_three_and_five():
   assert fizzbuzz.is_divisible_by(18,15) == False
   assert fizzbuzz.is_divisible_by(20,15) == False
+  
+def test_says_fizz():
+  assert fizzbuzz.says(3) == 'fizz'
+
+def test_says_buzz():
+  assert fizzbuzz.says(5) == 'buzz'
+
+def test_says_fizzbuzz():
+  assert fizzbuzz.says(15) == 'fizzbuzz'
      
