889cfc1619530af3f3a3bdf54f6a88ac008fbeb9
Maria Valero <mvalerogonzalez@gmail.com>
2021-10-29 12:25:45 +0000

    Six tests working - full refactoring

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,3 @@
 
-def is_divisible_by_three(number):
-  return number % 3 == 0
-  
-def is_divisible_by_five(number):
-  return number % 5 == 0
-  
-def is_divisible_by_three_and_five(number):
-  return number % 3 == 0 and number % 5 == 0
+def is_divisible_by(number, divisor):
+     return number % divisor == 0
--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -1,21 +1,21 @@
 import fizzbuzz
 
 def test_that_number_is_divisible_by_three():
-  assert fizzbuzz.is_divisible_by_three(3) == True
+  assert fizzbuzz.is_divisible_by(3,3) == True
 
 def test_that_number_is_NOT_divisible_by_three():
-  assert fizzbuzz.is_divisible_by_three(1) == False
+  assert fizzbuzz.is_divisible_by(1,3) == False
   
 def test_that_number_is_divisible_by_five():
-  assert fizzbuzz.is_divisible_by_five(5) == True
+  assert fizzbuzz.is_divisible_by(5,5) == True
   
 def test_that_number_is_NOT_divisible_by_five():
-  assert fizzbuzz.is_divisible_by_three(2) == False
+  assert fizzbuzz.is_divisible_by(2,5) == False
      
 def test_that_number_is_divisible_by_three_and_five():
-  assert fizzbuzz.is_divisible_by_three_and_five(15) == True
+  assert fizzbuzz.is_divisible_by(15,15) == True
      
 def test_that_number_is_NOT_divisible_by_three_and_five():
-  assert fizzbuzz.is_divisible_by_three_and_five(1) == False
-  assert fizzbuzz.is_divisible_by_three_and_five(3) == False
+  assert fizzbuzz.is_divisible_by(18,15) == False
+  assert fizzbuzz.is_divisible_by(20,15) == False
      
