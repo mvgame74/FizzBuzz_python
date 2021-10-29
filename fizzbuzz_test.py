0c5441016abf69bcb275fff953c2e07f4e8885f8
Maria Valero <mvalerogonzalez@gmail.com>
2021-10-29 12:10:13 +0000

    Six basic tests working

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -8,8 +8,14 @@ def test_that_number_is_NOT_divisible_by_three():
   
 def test_that_number_is_divisible_by_five():
   assert fizzbuzz.is_divisible_by_five(5) == True
+  
+def test_that_number_is_NOT_divisible_by_five():
+  assert fizzbuzz.is_divisible_by_three(2) == False
      
 def test_that_number_is_divisible_by_three_and_five():
   assert fizzbuzz.is_divisible_by_three_and_five(15) == True
      
+def test_that_number_is_NOT_divisible_by_three_and_five():
+  assert fizzbuzz.is_divisible_by_three_and_five(1) == False
+  assert fizzbuzz.is_divisible_by_three_and_five(3) == False
      
